8fb7699f5528de6567bd21a50ed37e312d07b073
太行山的隐狼 <142863971+6syano@users.noreply.github.com>
2024-10-26 23:02:22 +0800

    Add files via upload

--- phira.py
+++ phira.py
@@ -10,25 +10,25 @@ for level in levels:
     os.mkdir("phira/%s" %level)
 
 infos = {}
-with open("info.tsv", encoding="utf8") as f:
+with open("info.csv", encoding="utf8") as f:
     while True:
         line = f.readline()
         if not line:
             break
-        line = line[:-1].split("\t")
+        line = line[:-1].split("\\")
         infos[line[0]] = {"Name": line[1], "Composer": line[2], "Illustrator": line[3], "Chater": line[4:]}
-with open("difficulty.tsv", encoding="utf8") as f:
+with open("difficulty.csv", encoding="utf8") as f:
     while True:
         line = f.readline()
         if not line:
             break
-        line = line[:-1].split("\t")
+        line = line[:-1].split(",")
         infos[line[0]]["difficulty"] = line[1:]
 for id, info in infos.items():
     print(info["Name"], info["Composer"])
     for level in range(len(info["difficulty"])):
         with ZipFile("phira/%s/%s-%s.pez" % (levels[level], id, levels[level]), "x") as pez:
-            pez.writestr("info.txt", "#\nName: %s\nSong: %s.ogg\nPicture: %s.png\nChart: %s.json\nLevel: %s Lv.%s\nComposer: %s\nIllustrator: %s\nCharter: %s" % (info["Name"], id, id, id, levels[level], info["difficulty"][level], info["Composer"], info["Illustrator"], info["Chater"][level]))
+            pez.writestr("info.txt", "#\nName: %s\nSong: %s.wav\nPicture: %s.png\nChart: %s.json\nLevel: %s Lv.%s\nComposer: %s\nIllustrator: %s\nCharter: %s" % (info["Name"], id, id, id, levels[level], info["difficulty"][level], info["Composer"], info["Illustrator"], info["Chater"][level]))
             pez.write("Chart_%s/%s.0.json" % (levels[level], id), "%s.json" % id)
             pez.write("IllustrationLowRes/%s.png" % id, "%s.png" % id)
-            pez.write("music/%s.ogg" % id, "%s.ogg" % id)
+            pez.write("music/%s.wav" % id, "%s.wav" % id)
